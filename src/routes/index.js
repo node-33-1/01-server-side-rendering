81a0e2b8c528914890805d90533d721265ea2365
andres <andres.david.mm@hotmail.com>
2024-02-20 19:56:59 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const team = require('../team.json');
 
 // colocar las rutas aquí
 router.get('/', (req, res) => {
-    res.send("Welcome to express");
+    res.render("home");
 });
 
+router.get('/team', (req, res) => {
+    // console.log(team);
+    res.render("team", { team });
+});
+
+router.get('/team/:name', (req, res) => {
+    const { name } = req.params;
+    res.render("team-member", { name });
+});
+
+router.get('/product', (req, res) => {
+    res.send("Este es nuestro producto...");
+});
+
+router.get('/about-me', (req, res) => {
+    res.render("about-me");
+});
+
+router.get('/projects', (req, res) => {
+    res.render("projects");
+});
+
+router.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    res.render("project-detail", { id });
+})
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
